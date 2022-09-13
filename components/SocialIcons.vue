3b02e02caeea3ff9295fc13881456733ac3dc822
yzh990918 <251205668@qq.com>
2022-09-13 11:38:57 +0800

    chore: init project

---
+++ components/SocialIcons.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <NuxtLink target="_blank" href="https://github.com/vbs-plus" title="Visit  GitHub" class="group">
+      <i-line-md-github class="icon icon--off" alt="Github Logo" />
+      <i-line-md-github-loop class="icon icon--on" alt="Github Logo" />
+    </NuxtLink>
+  </div>
+</template>
+
+<style scoped>
+a {
+  @apply hover:text-gray-700 dark:hover:text-gray-300 p-2;
+}
+</style>
---
+++ logic/constants.ts
@@ -0,0 +1,11 @@
+export const SiteName = 'V B S'
+export const SiteUrl = 'https://vps.plus'
+export const SiteImage = '/cover.png'
+export const SiteLogo = '/cover'
+export const SiteLanguage = 'en-ZH'
+export const SiteTagLine = 'Open Source Orgaization'
+export const SiteDescription = 'Some Descriptions.'
+export const SameAs = [
+  '',
+  '',
+]
